df2548f09caffd18f2fcbdedf0d0a053f29cfd57
sftraining123 <131932231+sftraining123@users.noreply.github.com>
2023-05-02 15:11:03 -0500

    Update streamlit_app.py
    parsing json and creating tables

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# write your own comment -parsing json data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - creating table
+streamlit.dataframe(fruityvice_normalized)
