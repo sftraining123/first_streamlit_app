cee61e5a5137c6a5430d43c387ba4344d87fd889
sftraining123 <131932231+sftraining123@users.noreply.github.com>
2023-05-07 15:51:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLERROR
+# from urllib.error import URLERROR
 streamlit.title('My Parents New Healthy Diner')
